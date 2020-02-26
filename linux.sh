d54a539c9c757dc2b453cdbf1ce9baac67a1641a
r4nchy <42188371+alx-xlx@users.noreply.github.com>
2020-02-26 06:08:34 +0530

    linux:

--- linux.sh
+++ linux.sh
@@ -2,7 +2,6 @@ pip3 install requests
 pip3 install -U requests[socks]
 pip3 install bs4
 pip3 install lxml
-
 # torsocks for using tor connection
 # https://gitweb.torproject.org/torsocks.git
 # https://github.com/dgoulet/torsocks
