0f3d3a63c82930bcfe7c56100cd925917329c80f
r4nchy <42188371+alx-xlx@users.noreply.github.com>
2020-02-26 05:38:02 +0530

    --- Auto-Git Commit ---

--- deepnet.py
+++ deepnet.py
@@ -7,8 +7,8 @@ import urllib
 URL = "http://awaken4u2655myzx.onion/F3thinker%2001/"
 
 proxies = {
-    'http': 'socks5h://127.0.0.1:9050',
-    'https': 'socks5h://127.0.0.1:9050'
+    'http': 'socks5h://127.0.0.1:9050',        # Port 9050 if using tor.exe
+    'https': 'socks5h://127.0.0.1:9050'        # Port 9051 OR 9052 if using Tor Browser
 }
 
 root = requests.get(URL)
