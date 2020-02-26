ac2218154021e0fd209ea0103a7097b2404b2fcc
r4nchy <42188371+alx-xlx@users.noreply.github.com>
2020-02-26 06:08:29 +0530

    --- Auto-Git Commit ---

--- linux.sh
+++ linux.sh
@@ -3,4 +3,7 @@ pip3 install -U requests[socks]
 pip3 install bs4
 pip3 install lxml
 
+# torsocks for using tor connection
+# https://gitweb.torproject.org/torsocks.git
+# https://github.com/dgoulet/torsocks
 git clone https://git.torproject.org/torsocks.git
