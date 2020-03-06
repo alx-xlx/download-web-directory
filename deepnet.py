9089cdf5b17cf5e0f99906efa3a5bd00741d5fa9
r4nchy <42188371+alx-xlx@users.noreply.github.com>
2020-03-06 21:31:42 +0530

    --- Auto-Git Commit ---

--- deepnet.py
+++ deepnet.py
@@ -13,30 +13,3 @@ proxies = {
 
 root = requests.get(URL, proxies=proxies)
 soup = BeautifulSoup(root.content,'lxml')
-
-files = []
-dir = []
-# Gather all the files
-for img in soup.find_all('img',{'src':True}):
-    if (img['src'] == '/icons/blank.gif' or img['src'] == '/icons/back.gif' or img['src'] == '/icons/folder.gif'):
-        continue
-    if img['src'] == '/icons/folder.gif':
-        subddir_name = img.find_next('a')['href']
-        subddir_url = URL + subddir_name
-        dir.append(subddir)
-    files.append(img.find_next('a')['href'])
-
-    print(files)
-
-# Download those files
-
-for eachfile in range(len(files)):
-    download_link = URL + files[eachfile]              # Download LInk
-    filename = urllib.parse.unquote(files[eachfile])   # Filename
-
-    rawfile = requests.get(download_link)
-    with open(filename, 'wb') as f:
-        f.write(rawfile.content)
-
-    print(' ' + filename + ' Done !! ' + str(eachfile+1) + '/' +  len(files))
-
